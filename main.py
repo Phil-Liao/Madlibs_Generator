1e74beabd2f41593fa9a8ee82d9ca7cd412c36e5
Phil Liao <cheweiphil@gmail.com>
2023-07-13 16:09:57 +0800

    Test

--- main.py
+++ main.py
@@ -10,5 +10,22 @@ for i in readlines:
         if i[j] == '_':
             lst.append(j)
     print(lst)
-    for i in range(1, (lst%(3*2))+1, 1):
-        3 * (i * 2 -1)
+
+    print(len(lst)/6)
+    line = i
+    for j in range(1, int((len(lst)/6)+1)):
+        start = lst[(3*((j*2)-1))-1]+2
+        end = lst[(3*((j*2)-1))]-2
+        #print(i[start])
+        #print(i[end])
+        print(i[start:end+1])
+        if i[end].isnumeric():
+            answer = input("Enter a " + i[start:end] + ':')
+        else:
+            answer = input("Enter a " + i[start:end+1] + ':')
+        print("___("+i[start:end+1]+")___")
+        print(answer)
+        line = line.replace(f"___({i[start:end+1]})___", answer, 1)
+        print(line)
+    new_paragraph.append(line)
+print(new_paragraph)
